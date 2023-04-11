051d438b85a823a58c14001a60aa734325330148
sora <softsora@163.com>
2023-04-12 01:17:26 +0800

    refacter desc

--- src/g_render_system.cpp
+++ src/g_render_system.cpp
@@ -45,11 +45,20 @@ void RenderSystem::createUniformBuffers(uint32_t count)
 
 void RenderSystem::createDescriptorPool(uint32_t count)
 {
-
-    std::array<::vk::DescriptorPoolSize, 2> poolSizes{};
-    poolSizes[0] = ::vk::DescriptorPoolSize(::vk::DescriptorType::eUniformBuffer, count);
-    poolSizes[1] = ::vk::DescriptorPoolSize(::vk::DescriptorType::eCombinedImageSampler, count);
-
+    count = 1000;
+    std::array<::vk::DescriptorPoolSize, 11> poolSizes{
+        ::vk::DescriptorPoolSize(::vk::DescriptorType::eCombinedImageSampler, count),
+        ::vk::DescriptorPoolSize(::vk::DescriptorType::eSampler, count),
+        ::vk::DescriptorPoolSize(::vk::DescriptorType::eSampledImage, count),
+        ::vk::DescriptorPoolSize(::vk::DescriptorType::eStorageImage, count),
+        ::vk::DescriptorPoolSize(::vk::DescriptorType::eUniformTexelBuffer, count),
+        ::vk::DescriptorPoolSize(::vk::DescriptorType::eStorageTexelBuffer, count),
+        ::vk::DescriptorPoolSize(::vk::DescriptorType::eUniformBuffer, count),
+        ::vk::DescriptorPoolSize(::vk::DescriptorType::eStorageBuffer, count),
+        ::vk::DescriptorPoolSize(::vk::DescriptorType::eUniformBufferDynamic, count),
+        ::vk::DescriptorPoolSize(::vk::DescriptorType::eStorageBufferDynamic, count),
+        ::vk::DescriptorPoolSize(::vk::DescriptorType::eInputAttachment, count)
+    };
     
     ::vk::DescriptorPoolCreateInfo createInfo;
     createInfo.setPoolSizes(poolSizes)
