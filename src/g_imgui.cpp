548b96f58961eefd9b69565927056b422aa619b6
wxc <softsora@163.com>
2023-11-06 16:28:04 +0800

    rebuild imgui

---
+++ src/g_imgui.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ziyu on 2023/11/6:0006.
+//
+
+#include "g_imgui.hpp"
---
+++ src/g_imgui.hpp
@@ -0,0 +1,10 @@
+//
+// Created by ziyu on 2023/11/6:0006.
+//
+
+#ifndef GRAPHICS_G_IMGUI_HPP
+#define GRAPHICS_G_IMGUI_HPP
+
+class g_imgui {};
+
+#endif  // GRAPHICS_G_IMGUI_HPP
