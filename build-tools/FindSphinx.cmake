5c4481e1b28a4e943a88f6df1abaef1e7edd71d5
wxc <softsora@163.com>
2024-12-17 18:23:36 +0800

    修改cmake 路径

--- build-tools/FindSphinx.cmake
+++
@@ -1,9 +0,0 @@
-find_program(
-  SPHINX_EXECUTABLE
-  NAMES sphinx-build
-  DOC "Path to sphinx-build executable")
-
-# handle REQUIRED and QUIET arguments, set SPHINX_FOUND variable
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(
-  Sphinx "Unable to locate sphinx-build executable" SPHINX_EXECUTABLE)
