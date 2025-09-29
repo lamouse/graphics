add2c824dc8fed71e957741a5dc18dec444aecbc
wxc <softsora@163.com>
2025-09-29 19:08:42 +0800

    为Entity添加一个bool函数

--- src/ecs/scene/entity.hpp
+++ src/ecs/scene/entity.hpp
@@ -10,42 +10,44 @@
 namespace ecs {
 
 class Entity {
-    friend class Scene;
+        friend class Scene;
+
     public:
         CLASS_DEFAULT_COPYABLE(Entity);
         CLASS_DEFAULT_MOVEABLE(Entity);
         Entity() = default;
         ~Entity() = default;
-        template<typename  T>
-        [[nodiscard]] auto hasComponent()const -> bool {
+        template <typename T>
+        [[nodiscard]] auto hasComponent() const -> bool {
             return scene_->registry_.all_of<T>(handle_);
         }
-        template<typename T, typename... Args>
+        template <typename T, typename... Args>
         auto addComponent(Args&&... args) -> T& {
             // NOLINTNEXTLINE(modernize-use-trailing-return-type)
             ASSERT_MSG(!hasComponent<T>(), "Entity already has component");
             return scene_->registry_.emplace<T>(handle_, std::forward<Args>(args)...);
         }
 
-        template<typename T>
+        template <typename T>
         auto getComponent() -> T& {
             // NOLINTNEXTLINE(modernize-use-trailing-return-type)
             ASSERT_MSG(hasComponent<T>(), "Entity dose not has component");
             return scene_->registry_.get<T>(handle_);
         }
 
-        template<typename T>
+        template <typename T>
         void removeComponent() {
             // NOLINTNEXTLINE(modernize-use-trailing-return-type)
             ASSERT_MSG(!hasComponent<T>(), "Entity dose not has component");
             scene_->registry_.remove<T>(handle_);
         }
+        // NOLINTNEXTLINE(hicpp-explicit-conversions)
+        operator bool() const { return handle_ != entt::null; }
 
     private:
         Entity(entt::entity handle, Scene* scene) : handle_(handle), scene_(scene) {};
-        entt::entity handle_;
-        Scene* scene_;
+        entt::entity handle_{entt::null};
+        Scene* scene_{nullptr};
 };
 
 }  // namespace ecs
-
