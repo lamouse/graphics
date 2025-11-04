d2d6deb6b51da8383bd840b604fa1d50bc88e13c
wxc <softsora@163.com>
2025-11-04 11:54:32 +0800

    修改宏定义

--- src/render_core/vulkan_common/vulkan_common.hpp
+++ src/render_core/vulkan_common/vulkan_common.hpp
@@ -3,8 +3,10 @@
 #ifdef _WIN32
 #define VK_USE_PLATFORM_WIN32_KHR
 #include <windows.h>
-
+#include <vulkan/vulkan_win32.h>
 #elif defined(__APPLE__)
+#include <MoltenVK/mvk_vulkan.h>
+#include <vulkan/vulkan_metal.h>
 #define VK_USE_PLATFORM_METAL_EXT
 #elif defined(__ANDROID__)
 #define VK_USE_PLATFORM_ANDROID_KHR
@@ -14,13 +16,4 @@
 #include <vulkan/vulkan_xlib.h>
 #define VK_USE_PLATFORM_WAYLAND_KHR
 #include <vulkan/vulkan_wayland.h>
-#endif
-
-#ifdef __APPLE__
-#include <MoltenVK/mvk_vulkan.h>
-#include <vulkan/vulkan_metal.h>
-#elif defined(_WIN32)
-#include <vulkan/vulkan_win32.h>
-#else
-
 #endif
