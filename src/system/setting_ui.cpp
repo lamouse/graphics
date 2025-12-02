e808e08c11ffbe2ad02fb32ccd27ce75b5655816
wxc <softsora@163.com>
2025-12-02 17:43:02 +0800

    修改log setting

--- src/system/setting_ui.cpp
+++ src/system/setting_ui.cpp
@@ -9,7 +9,15 @@
 
 // module setting;
 namespace {
-
+auto to_bool(const std::string& s) -> bool {
+    if (s == "1" || s == "true" || s == "True" || s == "yes") {
+        return true;
+    }
+    if (s == "0" || s == "false" || s == "False" || s == "no") {
+        return false;
+    }
+    throw std::invalid_argument("Invalid bool string: " + s);
+};
 void vsync_setting() {
     auto canon = settings::enums::EnumMetadata<settings::enums::VSyncMode>::canonicalizations();
     std::vector<const char*> names;
@@ -24,6 +32,9 @@ void vsync_setting() {
 }
 
 void log_settings() {
+    ImGui::Separator();
+    ImGui::Text("log setting");
+
     auto canon = settings::enums::EnumMetadata<settings::enums::LogLevel>::canonicalizations();
     std::vector<const char*> names;
     for (auto& key : canon | std::views::keys) {
@@ -34,6 +45,46 @@ void log_settings() {
     ImGui::Combo("log level", &item_current, names.data(), static_cast<int>(names.size()));
     const auto level = settings::enums::ToEnum<settings::enums::LogLevel>(names[item_current]);
     settings::values.log_level.SetValue(level);
+
+    auto& render_category =
+        settings::values.linkage.by_category.find(settings::Category::log)->second;
+    for (auto* setting : render_category) {
+        if (setting->TypeId() == std::type_index(typeid(bool))) {
+            auto value_string = setting->ToString();
+            bool tmp = to_bool(value_string);
+            if (!setting->RuntimeModifiable()) {
+                ImGui::BeginDisabled(true);  // 禁用交互
+            }
+            ImGui::Checkbox(setting->GetLabel().c_str(), &tmp);
+            if (!setting->RuntimeModifiable()) {
+                ImGui::EndDisabled();
+            } else {
+                dynamic_cast<settings::Setting<bool, false>*>(setting)->SetValue(tmp);
+            }
+        }
+    }
+}
+
+void render_setting() {
+    ImGui::Separator();
+    ImGui::Text("render setting");
+    auto& render_category =
+        settings::values.linkage.by_category.find(settings::Category::render)->second;
+    for (auto* setting : render_category) {
+        if (setting->TypeId() == std::type_index(typeid(bool))) {
+            auto value_string = setting->ToString();
+            bool tmp = to_bool(value_string);
+            if (!setting->RuntimeModifiable()) {
+                ImGui::BeginDisabled(true);  // 禁用交互
+            }
+            ImGui::Checkbox(setting->GetLabel().c_str(), &tmp);
+            if (!setting->RuntimeModifiable()) {
+                ImGui::EndDisabled();
+            } else {
+                dynamic_cast<settings::Setting<bool, false>*>(setting)->SetValue(tmp);
+            }
+        }
+    }
 }
 
 }  // namespace
@@ -43,34 +94,7 @@ void draw_setting(bool& show) {
         ImGui::Begin("\ueb51 系统设置", &show);
         vsync_setting();
         log_settings();
-
-        auto to_bool = [](const std::string& s) {
-            if (s == "1" || s == "true" || s == "True" || s == "yes") {
-                return true;
-            }
-            if (s == "0" || s == "false" || s == "False" || s == "no") {
-                return false;
-            }
-            throw std::invalid_argument("Invalid bool string: " + s);
-        };
-        ImGui::Text("render setting");
-        auto& render_category =
-            settings::values.linkage.by_category.find(settings::Category::render)->second;
-        for (auto* setting : render_category) {
-            if (setting->TypeId() == std::type_index(typeid(bool))) {
-                auto value_string = setting->ToString();
-                bool tmp = to_bool(value_string);
-                if (!setting->RuntimeModifiable()) {
-                    ImGui::BeginDisabled(true);  // 禁用交互
-                }
-                ImGui::Checkbox(setting->GetLabel().c_str(), &tmp);
-                if (!setting->RuntimeModifiable()) {
-                    ImGui::EndDisabled();
-                } else {
-                    dynamic_cast<settings::Setting<bool, false>*>(setting)->SetValue(tmp);
-                }
-            }
-        }
+        render_setting();
 
         ImGui::End();
     }
