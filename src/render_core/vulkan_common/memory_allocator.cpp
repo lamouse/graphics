b4f44cac59edd4e3f72395fa863a8392d86cbb6e
wxc <softsora@163.com>
2026-01-07 12:37:36 +0800

    删除不需要的代码

--- src/render_core/vulkan_common/memory_allocator.cpp
+++ src/render_core/vulkan_common/memory_allocator.cpp
@@ -1,6 +1,7 @@
 #include "memory_allocator.hpp"
 #include "common/alignment.h"
 #include <spdlog/spdlog.h>
+#include <algorithm>
 #include <optional>
 #include <cassert>
 #include "common/literals.hpp"
@@ -24,7 +25,7 @@ struct Range {
         0x3200ULL << 10,  0x4000ULL << 10,  0x6000ULL << 10,  0x8000ULL << 10, 0xA000ULL << 10,
         0x10000ULL << 10, 0x18000ULL << 10, 0x20000ULL << 10,
     };
-    static_assert(std::is_sorted(sizes.begin(), sizes.end()));
+    static_assert(std::ranges::is_sorted(sizes));
 
     const auto it = std::ranges::lower_bound(sizes, required_size);
     return it != sizes.end() ? *it : common::AlignUp(required_size, 4ULL << 20);
@@ -80,135 +81,6 @@ struct Range {
 }
 }  // namespace
 
-class MemoryAllocation {
-    public:
-        explicit MemoryAllocation(MemoryAllocator* const allocator_, DeviceMemory memory_,
-                                  vk::MemoryPropertyFlags properties, u64 allocation_size_,
-                                  u32 type)
-            : allocator{allocator_},
-              memory{std::move(memory_)},
-              allocation_size{allocation_size_},
-              property_flags{properties},
-              shifted_memory_type{1U << type} {}
-
-        CLASS_NON_COPYABLE(MemoryAllocation);
-        CLASS_NON_MOVEABLE(MemoryAllocation);
-
-        [[nodiscard]] auto commit(vk::DeviceSize size, vk::DeviceSize alignment)
-            -> std::optional<MemoryCommit> {
-            const std::optional<u64> alloc = findFreeRegion(size, alignment);
-            if (!alloc) {
-                // Signal out of memory, it'll try to do more allocations.
-                return std::nullopt;
-            }
-            const Range range{
-                .begin = *alloc,
-                .end = *alloc + size,
-            };
-            commits.insert(std::ranges::upper_bound(commits, *alloc, {}, &Range::begin), range);
-            return std::make_optional<MemoryCommit>(this, *memory, *alloc, *alloc + size);
-        }
-
-        void free(u64 begin) {
-            const auto it = std::ranges::find(commits, begin, &Range::begin);
-            assert(it != commits.end() && "Invalid commit");
-            commits.erase(it);
-            if (commits.empty()) {
-                // Do not call any code involving 'this' after this call, the object will be
-                // destroyed
-                allocator->releaseMemory(this);
-            }
-        }
-
-        [[nodiscard]] auto map() -> std::span<u8> {
-            if (memory_mapped_span.empty()) {
-                u8* data = memory.map(0, allocation_size);
-                memory_mapped_span = std::span<u8>(data, allocation_size);
-            }
-            return memory_mapped_span;
-        }
-
-        /// Returns whether this allocation is compatible with the arguments.
-        [[nodiscard]] auto isCompatible(vk::MemoryPropertyFlags flags, u32 type_mask) const
-            -> bool {
-            return (flags & property_flags) == flags && (type_mask & shifted_memory_type) != 0;
-        }
-
-    private:
-        [[nodiscard]] static constexpr auto shiftType(u32 type) -> u32 { return 1U << type; }
-
-        [[nodiscard]] auto findFreeRegion(u64 size, u64 alignment) noexcept -> std::optional<u64> {
-            assert(std::has_single_bit(alignment));
-            const u64 alignment_log2 = std::countr_zero(alignment);
-            std::optional<u64> candidate;
-            u64 iterator = 0;
-            auto commit = commits.begin();
-            while (iterator + size <= allocation_size) {
-                candidate = candidate.value_or(iterator);
-                if (commit == commits.end()) {
-                    break;
-                }
-                if (commit->contains(*candidate, size)) {
-                    candidate = std::nullopt;
-                }
-                iterator = common::AlignUpLog2(commit->end, alignment_log2);
-                ++commit;
-            }
-            return candidate;
-        }
-
-        MemoryAllocator* const allocator;              ///< Parent memory allocation.
-        const DeviceMemory memory;                     ///< Vulkan memory allocation handler.
-        const u64 allocation_size;                     ///< Size of this allocation.
-        const vk::MemoryPropertyFlags property_flags;  ///< Vulkan memory property flags.
-        const u32 shifted_memory_type;                 ///< Shifted Vulkan memory type.
-        std::vector<Range> commits;        ///< All commit ranges done from this allocation.
-        std::span<u8> memory_mapped_span;  ///< Memory mapped span. Empty if not queried before.
-};
-
-MemoryCommit::MemoryCommit(MemoryAllocation* allocation, vk::DeviceMemory memory, u64 begin,
-                           u64 end) noexcept
-    : allocation_{allocation}, memory_{memory}, begin_{begin}, end_{end} {}
-
-MemoryCommit::MemoryCommit(VmaAllocator alloc, VmaAllocation a,
-                           const VmaAllocationInfo& info) noexcept
-    : allocator{alloc},
-      allocation{a},
-      memory_{info.deviceMemory},
-      begin_{info.offset},
-      end_{info.size},
-      mapped_ptr{info.pMappedData} {}
-
-MemoryCommit::~MemoryCommit() { release(); }
-auto MemoryCommit::operator=(MemoryCommit&& rhs) noexcept -> MemoryCommit& {
-    release();
-    allocation_ = std::exchange(rhs.allocation_, nullptr);
-    memory_ = rhs.memory_;
-    begin_ = rhs.begin_;
-    end_ = rhs.end_;
-    span_ = std::exchange(rhs.span_, std::span<u8>{});
-    return *this;
-}
-
-MemoryCommit::MemoryCommit(MemoryCommit&& rhs) noexcept
-    : allocation_{std::exchange(rhs.allocation_, nullptr)},
-      memory_{rhs.memory_},
-      begin_{rhs.begin_},
-      end_{rhs.end_},
-      span_{std::exchange(rhs.span_, std::span<u8>{})} {}
-
-auto MemoryCommit::map() -> std::span<u8> {
-    if (span_.empty()) {
-        span_ = allocation_->map().subspan(begin_, end_ - begin_);
-    }
-    return span_;
-}
-
-void MemoryCommit::release() {
-    if (allocation_) {
-        allocation_->free(begin_);
-    }
-}
 MemoryAllocator::MemoryAllocator(const Device& device_)
     : device{device_},
       allocator{device_.getAllocator()},
@@ -279,145 +151,6 @@ auto MemoryAllocator::createBuffer(const VkBufferCreateInfo& ci, MemoryUsage usa
     const bool is_coherent = property_flags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
 
     return Buffer(handle, device.getLogical(), allocator, allocation, mapped_data, is_coherent);
-}
-
-auto MemoryAllocator::commit(const vk::MemoryRequirements& requirements, MemoryUsage usage)
-    -> MemoryCommit {
-    // Find the fastest memory flags we can afford with the current requirements
-    const u32 type_mask = requirements.memoryTypeBits;
-    const vk::MemoryPropertyFlags usage_flags = memoryUsagePropertyFlags(usage);
-    const vk::MemoryPropertyFlags flags = memoryPropertyFlags(type_mask, usage_flags);
-    if (std::optional<MemoryCommit> commit = tryCommit(requirements, flags)) {
-        return std::move(*commit);
-    }
-    // Commit has failed, allocate more memory.
-    const u64 chunk_size = allocationChunkSize(requirements.size);
-    if (!tryAllocMemory(flags, type_mask, chunk_size)) {
-        // TODO(Rodrigo): Handle out of memory situations in some way like flushing to guest memory.
-        throw utils::VulkanException(VK_ERROR_OUT_OF_DEVICE_MEMORY);
-    }
-    // Commit again, this time it won't fail since there's a fresh allocation above.
-    // If it does, there's a bug.
-    return tryCommit(requirements, flags).value();
-}
-
-auto MemoryAllocator::tryAllocMemory(vk::MemoryPropertyFlags flags, u32 type_mask, u64 size)
-    -> bool {
-    const u32 type = findType(flags, type_mask).value();
-    DeviceMemory memory = device.logical().tryAllocateMemory({
-        .sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
-        .pNext = nullptr,
-        .allocationSize = size,
-        .memoryTypeIndex = type,
-    });
-    if (!memory) {
-        if ((flags & vk::MemoryPropertyFlagBits::eDeviceLocal) ==
-            vk::MemoryPropertyFlagBits::eDeviceLocal) {
-            // Try to allocate non device local memory
-            return tryAllocMemory(flags & ~vk::MemoryPropertyFlagBits::eDeviceLocal, type_mask,
-                                  size);
-        } else {
-            // RIP
-            return false;
-        }
-    }
-    allocations.push_back(
-        std::make_unique<MemoryAllocation>(this, std::move(memory), flags, size, type));
-    return true;
-}
-
-void MemoryAllocator::releaseMemory(MemoryAllocation* alloc) {
-    const auto it = std::ranges::find(allocations, alloc, &std::unique_ptr<MemoryAllocation>::get);
-    assert(it != allocations.end());
-    allocations.erase(it);
-}
-auto MemoryAllocator::tryCommit(const vk::MemoryRequirements& requirements,
-                                vk::MemoryPropertyFlags flags) -> std::optional<MemoryCommit> {
-    for (auto& allocation : allocations) {
-        if (!allocation->isCompatible(flags, requirements.memoryTypeBits)) {
-            continue;
-        }
-        if (auto commit = allocation->commit(requirements.size, requirements.alignment)) {
-            return commit;
-        }
-    }
-    if ((flags & vk::MemoryPropertyFlagBits::eDeviceLocal) ==
-        vk::MemoryPropertyFlagBits::eDeviceLocal) {
-        // Look for non device local commits on failure
-        return tryCommit(requirements, flags & ~vk::MemoryPropertyFlagBits::eDeviceLocal);
-    }
-    return std::nullopt;
-}
-
-auto MemoryAllocator::memoryPropertyFlags(u32 type_mask, vk::MemoryPropertyFlags flags) const
-    -> vk::MemoryPropertyFlags {
-    if (findType(flags, type_mask)) {
-        // Found a memory type with those requirements
-        return flags;
-    }
-    if ((flags & vk::MemoryPropertyFlagBits::eHostCached) ==
-        vk::MemoryPropertyFlagBits::eHostCached) {
-        // Remove host cached bit in case it's not supported
-        return memoryPropertyFlags(type_mask, flags & ~vk::MemoryPropertyFlagBits::eHostCached);
-    }
-    if ((flags & vk::MemoryPropertyFlagBits::eDeviceLocal) ==
-        vk::MemoryPropertyFlagBits::eDeviceLocal) {
-        // Remove device local, if it's not supported by the requested resource
-        return memoryPropertyFlags(type_mask, flags & ~vk::MemoryPropertyFlagBits::eDeviceLocal);
-    }
-    assert(false && "No compatible memory types found");
-    return {};
-}
-
-auto MemoryAllocator::findType(vk::MemoryPropertyFlags flags, u32 type_mask) const
-    -> std::optional<u32> {
-    for (u32 type_index = 0; type_index < properties.memoryTypeCount; ++type_index) {
-        const vk::MemoryPropertyFlags type_flags = properties.memoryTypes[type_index].propertyFlags;
-        if ((type_mask & (1U << type_index)) != 0 && (type_flags & flags) == flags) {
-            // The type matches in type and in the wanted properties.
-            return type_index;
-        }
-    }
-    // Failed to find index
-    return std::nullopt;
-}
-
-auto MemoryAllocator::commit(const vk::Buffer& buffer, MemoryUsage usage) -> MemoryCommit {
-    // Allocate memory appropriate for this buffer automatically
-    const auto vma_usage = memoryUsageVma(usage);
-
-    VmaAllocationCreateInfo ci{};
-    ci.flags = VMA_ALLOCATION_CREATE_WITHIN_BUDGET_BIT | memoryUsageVmaFlags(usage);
-    ci.usage = vma_usage;
-    ci.requiredFlags = 0;
-    ci.preferredFlags = static_cast<VkMemoryPropertyFlags>(memoryUsagePreferredVmaFlags(usage));
-    ci.pool = VK_NULL_HANDLE;
-    ci.pUserData = nullptr;
-    ci.priority = 0.0f;
-
-    const VkBuffer raw = buffer.operator VkBuffer();
-
-    VmaAllocation a{};
-    VmaAllocationInfo info{};
-
-    // Let VMA infer memory requirements from the buffer
-    VkResult res = vmaAllocateMemoryForBuffer(allocator, raw, &ci, &a, &info);
-
-    if (res != VK_SUCCESS) {
-        auto ci2 = ci;
-        ci2.flags &= ~VMA_ALLOCATION_CREATE_WITHIN_BUDGET_BIT;
-        res = vmaAllocateMemoryForBuffer(allocator, raw, &ci2, &a, &info);
-
-        if (res != VK_SUCCESS && (ci.preferredFlags & VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT)) {
-            auto ci3 = ci2;
-            ci3.preferredFlags &= ~VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
-            res = vmaAllocateMemoryForBuffer(allocator, raw, &ci3, &a, &info);
-        }
-    }
-
-    utils::check(res);
-    utils::check(vmaBindBufferMemory2(allocator, a, 0, raw, nullptr));
-    return MemoryCommit(allocator, a, info);
 }
 
 }  // namespace render::vulkan
--- src/render_core/vulkan_common/memory_allocator.hpp
+++ src/render_core/vulkan_common/memory_allocator.hpp
@@ -1,19 +1,13 @@
 #pragma once
 
-#include <memory>
-#include <span>
-#include <vector>
 #include "common/common_types.hpp"
 #include "render_core/vulkan_common/device.hpp"
-#include "common/common_funcs.hpp"
 #include "render_core/vulkan_common/vulkan_wrapper.hpp"
 #include "vma.hpp"
 
 VK_DEFINE_HANDLE(VmaAllocator)
 namespace render::vulkan {
 
-class MemoryMap;
-class MemoryAllocation;
 /// Hints and requirements for the backing memory type of a commit
 enum class MemoryUsage {
     DeviceLocal,  ///< Requests device local host visible buffer, falling back to device local
@@ -34,48 +28,6 @@ void ForEachDeviceLocalHostVisibleHeap(const Device& device, F&& f) {
     }
 }
 
-/// Ownership handle of a memory commitment.
-/// Points to a subregion of a memory allocation.
-class MemoryCommit {
-    public:
-        explicit MemoryCommit() noexcept = default;
-        explicit MemoryCommit(MemoryAllocation* allocation_, vk::DeviceMemory memory_, u64 begin_,
-                              u64 end_) noexcept;
-        MemoryCommit(VmaAllocator allocator, VmaAllocation allocation,
-                     const VmaAllocationInfo &info);
-        ~MemoryCommit();
-
-        auto operator=(MemoryCommit&&) noexcept -> MemoryCommit&;
-        MemoryCommit(MemoryCommit&&) noexcept;
-
-        auto operator=(const MemoryCommit&) -> MemoryCommit& = delete;
-        MemoryCommit(const MemoryCommit&) = delete;
-
-        /// Returns a host visible memory map.
-        /// It will map the backing allocation if it hasn't been mapped before.
-        auto map() -> std::span<u8>;
-
-        /// Returns the Vulkan memory handler.
-        [[nodiscard]] auto memory() const -> vk::DeviceMemory { return memory_; }
-
-        /// Returns the start position of the commit relative to the allocation.
-        [[nodiscard]] auto offset() const -> vk::DeviceSize {
-            return static_cast<vk::DeviceSize>(begin_);
-        }
-
-    private:
-        void release();
-
-        MemoryAllocation* allocation_{};  ///< Pointer to the large memory allocation.
-        VmaAllocator allocator{};   ///< VMA allocator
-        VmaAllocation allocation{};  ///< VMA allocation handle
-        vk::DeviceMemory memory_;         ///< Vulkan device memory handler.
-        u64 begin_{};                     ///< Beginning offset in bytes to where the commit exists.
-        u64 end_{};                       ///< Offset in bytes where the commit ends.
-        std::span<u8> span_;  ///< Host visible memory span. Empty if not queried before.
-        void *mapped_ptr{};  ///< Optional persistent mapped pointer
-};
-
 /// Memory allocator container.
 /// Allocates and releases memory allocations on demand.
 class MemoryAllocator {
@@ -91,50 +43,20 @@ class MemoryAllocator {
          */
         explicit MemoryAllocator(const Device& device_);
         ~MemoryAllocator();
-
-        CLASS_NON_COPYABLE(MemoryAllocator);
+        MemoryAllocator(const MemoryAllocator&) = delete;
+        MemoryAllocator(MemoryAllocator&&) = delete;
+        auto operator=(const MemoryAllocator&) = delete;
+        auto operator=(MemoryAllocator&&) = delete;
 
         [[nodiscard]] auto createImage(const VkImageCreateInfo& ci) const -> Image;
 
         [[nodiscard]] auto createBuffer(const VkBufferCreateInfo& ci, MemoryUsage usage) const
             -> Buffer;
 
-        /**
-         * Commits a memory with the specified requirements.
-         *
-         * @param requirements Requirements returned from a Vulkan call.
-         * @param usage        Indicates how the memory will be used.
-         *
-         * @returns A memory commit.
-         */
-        auto commit(const vk::MemoryRequirements& requirements, MemoryUsage usage) -> MemoryCommit;
-
-        /// Commits memory required by the buffer and binds it.
-        auto commit(const vk::Buffer& buffer, MemoryUsage usage) -> MemoryCommit;
-
     private:
-        /// Tries to allocate a chunk of memory.
-        auto tryAllocMemory(vk::MemoryPropertyFlags flags, u32 type_mask, u64 size) -> bool;
-
-        /// Releases a chunk of memory.
-        void releaseMemory(MemoryAllocation* alloc);
-
-        /// Tries to allocate a memory commit.
-        auto tryCommit(const vk::MemoryRequirements& requirements, vk::MemoryPropertyFlags flags)
-            -> std::optional<MemoryCommit>;
-
-        /// Returns the fastest compatible memory property flags from the wanted flags.
-        [[nodiscard]] auto memoryPropertyFlags(u32 type_mask, vk::MemoryPropertyFlags flags) const
-            -> vk::MemoryPropertyFlags;
-
-        /// Returns index to the fastest memory type compatible with the passed requirements.
-        [[nodiscard]] auto findType(vk::MemoryPropertyFlags flags, u32 type_mask) const
-            -> std::optional<u32>;
-
         const Device& device;                                 ///< Device handle.
         VmaAllocator allocator;                               ///< Vma allocator.
         const vk::PhysicalDeviceMemoryProperties properties;  ///< Physical device properties.
-        std::vector<std::unique_ptr<MemoryAllocation>> allocations;  ///< Current allocations.
         VkDeviceSize buffer_image_granularity;  // The granularity for adjacent offsets between
                                                 // buffers and optimal images
         u32 valid_memory_types{~0u};
