a5494f207d81d48ad8cb21ff76f6932c73eb42c2
wxc <softsora@163.com>
2026-01-07 15:48:35 +0800

    fix gcc build error

--- src/resource/texture/ktx_image.cpp
+++ src/resource/texture/ktx_image.cpp
@@ -3,15 +3,15 @@
 namespace resource::image {
 KtxImage::KtxImage(const std::string& path) {
     KTX_error_code result = ktxTexture_CreateFromNamedFile(
-        path.c_str(), KTX_TEXTURE_CREATE_LOAD_IMAGE_DATA_BIT, &ktxTexture);
+        path.c_str(), KTX_TEXTURE_CREATE_LOAD_IMAGE_DATA_BIT, &handle);
 
     if (result != KTX_SUCCESS) {
         throw std::runtime_error("Failed to load KTX2 file");
     }
 }
 KtxImage::~KtxImage() {
-    if (ktxTexture) {
-        ktxTexture_Destroy(ktxTexture);
+    if (handle) {
+        ktxTexture_Destroy(handle);
     }
 }
 }  // namespace resource::image
