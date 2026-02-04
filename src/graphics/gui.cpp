6ec8bcd38bdbe5d94fd35938a767ef129792d947
wxc <softsora@163.com>
2026-02-04 16:52:07 +0800

    删除不必要的代码

--- src/graphics/gui.cpp
+++ src/graphics/gui.cpp
@@ -22,19 +22,7 @@
 #include <filesystem>
 
 #include <limits>
-// Helper to wire demo markers located in code to an interactive browser
-typedef void (*ImGuiDemoMarkerCallback)(const char* file, int line, const char* section,
-                                        void* user_data);
-extern ImGuiDemoMarkerCallback GImGuiDemoMarkerCallback;
-extern void* GImGuiDemoMarkerCallbackUserData;
-ImGuiDemoMarkerCallback GImGuiDemoMarkerCallback = NULL;
-void* GImGuiDemoMarkerCallbackUserData = NULL;
-#define IMGUI_DEMO_MARKER(section)                                      \
-    do {                                                                \
-        if (GImGuiDemoMarkerCallback != NULL)                           \
-            GImGuiDemoMarkerCallback(__FILE__, __LINE__, section,       \
-                                     GImGuiDemoMarkerCallbackUserData); \
-    } while (0)
+
 namespace {
 constexpr float OUTLINER_WIDTH = 400.f;
 constexpr float RENDER_STATUS_BAR_HEIGHT = 45.f;
@@ -42,9 +30,7 @@ ecs::Entity detail_entity;
 
 // Note that shortcuts are currently provided for display only
 // (future version will add explicit flags to BeginMenu() to request processing shortcuts)
-static void ShowExampleMenuFile() {
-    IMGUI_DEMO_MARKER("Examples/Menu");
-    ImGui::MenuItem("(demo menu)", NULL, false, false);
+ void ShowFileMenu() {
     if (ImGui::MenuItem("New")) {
     }
     if (ImGui::MenuItem("Open", "Ctrl+O")) {
@@ -54,75 +40,11 @@ static void ShowExampleMenuFile() {
         ImGuiFileDialog::Instance()->OpenDialog("ChooseFileDlgKey", "Choose File", ".*",
                                                 config);
     }
-    if (ImGui::BeginMenu("Open Recent")) {
-        ImGui::MenuItem("fish_hat.c");
-        ImGui::MenuItem("fish_hat.inl");
-        ImGui::MenuItem("fish_hat.h");
-        if (ImGui::BeginMenu("More..")) {
-            ImGui::MenuItem("Hello");
-            ImGui::MenuItem("Sailor");
-            if (ImGui::BeginMenu("Recurse..")) {
-                ShowExampleMenuFile();
-                ImGui::EndMenu();
-            }
-            ImGui::EndMenu();
-        }
-        ImGui::EndMenu();
-    }
     if (ImGui::MenuItem("Save", "Ctrl+S")) {
     }
     if (ImGui::MenuItem("Save As..")) {
     }
 
-    ImGui::Separator();
-    IMGUI_DEMO_MARKER("Examples/Menu/Options");
-    if (ImGui::BeginMenu("Options")) {
-        static bool enabled = true;
-        ImGui::MenuItem("Enabled", "", &enabled);
-        ImGui::BeginChild("child", ImVec2(0, 60), ImGuiChildFlags_Borders);
-        for (int i = 0; i < 10; i++) ImGui::Text("Scrolling Text %d", i);
-        ImGui::EndChild();
-        static float f = 0.5f;
-        static int n = 0;
-        ImGui::SliderFloat("Value", &f, 0.0f, 1.0f);
-        ImGui::InputFloat("Input", &f, 0.1f);
-        ImGui::Combo("Combo", &n, "Yes\0No\0Maybe\0\0");
-        ImGui::EndMenu();
-    }
-
-    IMGUI_DEMO_MARKER("Examples/Menu/Colors");
-    if (ImGui::BeginMenu("Colors")) {
-        float sz = ImGui::GetTextLineHeight();
-        for (int i = 0; i < ImGuiCol_COUNT; i++) {
-            const char* name = ImGui::GetStyleColorName((ImGuiCol)i);
-            ImVec2 p = ImGui::GetCursorScreenPos();
-            ImGui::GetWindowDrawList()->AddRectFilled(p, ImVec2(p.x + sz, p.y + sz),
-                                                      ImGui::GetColorU32((ImGuiCol)i));
-            ImGui::Dummy(ImVec2(sz, sz));
-            ImGui::SameLine();
-            ImGui::MenuItem(name);
-        }
-        ImGui::EndMenu();
-    }
-
-    // Here we demonstrate appending again to the "Options" menu (which we already created above)
-    // Of course in this demo it is a little bit silly that this function calls BeginMenu("Options")
-    // twice. In a real code-base using it would make senses to use this feature from very different
-    // code locations.
-    if (ImGui::BeginMenu("Options"))  // <-- Append!
-    {
-        IMGUI_DEMO_MARKER("Examples/Menu/Append to an existing menu");
-        static bool b = true;
-        ImGui::Checkbox("SomeOption", &b);
-        ImGui::EndMenu();
-    }
-
-    if (ImGui::BeginMenu("Disabled", false))  // Disabled
-    {
-        IM_ASSERT(0);
-    }
-    if (ImGui::MenuItem("Checked", nullptr, true)) {
-    }
     ImGui::Separator();
     if (ImGui::MenuItem("Quit", "Alt+F4")) {
     }
@@ -558,7 +480,7 @@ void show_menu(settings::MenuData& data) {
     }
     if (ImGui::BeginMainMenuBar()) {
         if (ImGui::BeginMenu("File")) {
-            ShowExampleMenuFile();
+            ShowFileMenu();
             ImGui::EndMenu();
         }
         if (ImGui::BeginMenu("Edit")) {
